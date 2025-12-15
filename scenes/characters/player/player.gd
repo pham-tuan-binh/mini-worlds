1a5763ea10f1b529a6fda8073c1ca529d91e3314
Binh <phamtuanbinh1504@gmail.com>
2025-12-15 19:13:35 +0100

    First

---
+++ scenes/characters/player/player.gd
@@ -0,0 +1,5 @@
+class_name Player
+extends CharacterBody2D
+
+var player_direction: Vector2 = Vector2.ZERO
+@export var current_tool: DataTypes.Tools = DataTypes.Tools.None
---
+++ scripts/globals/data_types.gd
@@ -0,0 +1,10 @@
+class_name DataTypes
+
+enum Tools {
+	None,
+	AxeWood,
+	TilledGround,
+	WaterCrops,
+	PlantCorn,
+	PlantTomato
+}

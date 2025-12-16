5e4dcea6d1a12e6a17a3f3b95924fcc319e98e87
Binh Pham <phamtuanbinh1504@gmail.com>
2025-12-16 01:40:54 +0100

    Ep 9

--- scenes/characters/player/chopping_state.gd
+++ scenes/characters/player/chopping_state.gd
@@ -2,6 +2,11 @@ extends NodeState
 
 @export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
+@export var hit_component_collision_shape: CollisionShape2D
+
+func _ready() -> void:
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2.ZERO
 
 func _on_process(_delta : float) -> void:
 	pass
@@ -9,24 +14,31 @@ func _on_process(_delta : float) -> void:
 func _on_physics_process(_delta : float) -> void:
 	pass
 
-
 func _on_next_transitions() -> void:
 	if !animated_sprite_2d.is_playing():
 		transition.emit("Idle")
 
 
 func _on_enter() -> void:
+	hit_component_collision_shape.disabled = false
+	
 	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("chopping_back")
+		hit_component_collision_shape.position = Vector2(0, -18)
 	elif player.player_direction  == Vector2.RIGHT:
 		animated_sprite_2d.play("chopping_right")
+		hit_component_collision_shape.position = Vector2(9, 0)
 	elif player.player_direction  == Vector2.LEFT:
 		animated_sprite_2d.play("chopping_left")
+		hit_component_collision_shape.position = Vector2(-9, 0)
 	elif player.player_direction  == Vector2.DOWN:
 		animated_sprite_2d.play("chopping_front")
+		hit_component_collision_shape.position = Vector2(0, 3)
 	else:
 		animated_sprite_2d.play("chopping_front")
+		hit_component_collision_shape.position = Vector2(0, 3)
 
 
 func _on_exit() -> void:
+	hit_component_collision_shape.disabled = true
 	animated_sprite_2d.stop()
